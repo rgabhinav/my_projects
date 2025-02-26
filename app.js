868427147d46843bda433058cceac577f884ed4a
rgabhinav <1920abhinav@gmail.com>
2025-02-26 18:16:18 +0530

    about config

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const db = require('./config/mongoose-connection')
+const config = require('config')
 
 require('dotenv').config();
 
@@ -21,6 +22,8 @@ app.get('/create', function(){
     res.send('New user created')
 });
 
-app.listen(process.env.PORT || port, () => {
-    console.log(`Server is running on port ${port}`);
-});
+// app.listen(process.env.PORT || port, () => {
+//     console.log(`Server is running on port ${port}`);
+// });
+
+app.listen(config.get("PORT")); //this is the second method using config
